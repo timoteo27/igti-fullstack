d0b31f2d8bd38c466dd4a962c6fad59d913de5ef
timoteo27 <timoteogrigull@gmail.com>
2020-07-26 19:29:12 -0300

    Início do desenvolvimento na questão 1

--- modulo_2/trabalho_pratico_1/index.js
+++ modulo_2/trabalho_pratico_1/index.js
@@ -1,1 +1,42 @@
-console.log('Hello World!');
+import { promises as fs } from 'fs';
+
+questao1();
+
+/* 
+ * 1. Criar uma função que irá criar um arquivo JSON para cada estado representado no
+  arquivo Estados.json, e o seu conteúdo será um array das cidades pertencentes a
+  aquele estado, de acordo com o arquivo Cidades.json. O nome do arquivo deve ser
+  o UF do estado, por exemplo: MG.json.2. Criar uma função que recebe como parâmetro o UF do estado, realize a leitura do
+  arquivo JSON correspondente e retorne a quantidade de cidades daquele estado.
+ */
+async function questao1() {
+  try {
+    const arrayEstados = JSON.parse(await fs.readFile('data/Estados.json'));
+    console.log(arrayEstados);
+  } catch (err) {
+    console.log(`Erro na questão 1! ${err}`);
+  }
+}
+
+/* Métodos que devem ser criados:
+  2. Criar uma função que recebe como parâmetro o UF do estado, realize a leitura do
+  arquivo JSON correspondente e retorne a quantidade de cidades daquele estado.
+  3. Criar um método que imprima no console um array com o UF dos cinco estados
+  que mais possuem cidades, seguidos da quantidade, em ordem decrescente. Você
+  pode usar a função criada no tópico 2. Exemplo de impressão: [“UF - 93”, “UF - 82”,
+  “UF - 74”, “UF - 72”, “UF - 65”]
+  4. Criar um método que imprima no console um array com o UF dos cinco estados
+  que menos possuem cidades, seguidos da quantidade, em ordem decrescente.
+  Você pode usar a função criada no tópico 2. Exemplo de impressão: [“UF - 30”, “UF
+  - 27”, “UF - 25”, “UF - 23”, “UF - 21”]
+  5. Criar um método que imprima no console um array com a cidade de maior nome de
+  cada estado, seguida de seu UF. Por exemplo: [“Nome da Cidade – UF”, “Nome da
+  Cidade – UF”, ...].
+  6. Criar um método que imprima no console um array com a cidade de menor nome
+  de cada estado, seguida de seu UF. Por exemplo: [“Nome da Cidade – UF”, “Nome
+  da Cidade – UF”, ...].
+  7. Criar um método que imprima no console a cidade de maior nome entre todos os
+  estados, seguido do seu UF. Exemplo: “Nome da Cidade - UF".
+  8. Criar um método que imprima no console a cidade de menor nome entre todos os
+  estados, seguido do seu UF. Exemplo: “Nome da Cidade - UF"
+*/
