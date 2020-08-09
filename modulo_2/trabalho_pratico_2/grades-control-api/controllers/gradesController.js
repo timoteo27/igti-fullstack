8057519fa78dd6514995d827148a850b50fe0a61
timoteo27 <timoteogrigull@gmail.com>
2020-08-09 14:57:24 -0300

    Project finished

--- modulo_2/trabalho_pratico_2/grades-control-api/controllers/gradesController.js
+++ modulo_2/trabalho_pratico_2/grades-control-api/controllers/gradesController.js
@@ -78,4 +78,67 @@ async function deletarGrade(idGrade) {
   }
 }
 
-export { inserirNovaGrade, buscaGrade, atualizarGrade, deletarGrade };
+async function buscaNotaAluno(student, subject) {
+  try {
+    const jsonData = JSON.parse(await fs.readFile(global.fileNameData));
+    const somaNota = jsonData.grades
+      .filter((g) => g.subject === subject && g.student === student)
+      .reduce(
+        (previousValue, currentValue) => previousValue + currentValue.value,
+        0
+      );
+    return {
+      message: `A soma da nota do aluno ${student} na matéria ${subject} é: ${somaNota}`,
+    };
+  } catch (err) {
+    console.log(err);
+    return { error: err.message };
+  }
+}
+
+async function buscaMediaSubjectType(subject, type) {
+  try {
+    const jsonData = JSON.parse(await fs.readFile(global.fileNameData));
+    const filteredArray = jsonData.grades.filter((g) => {
+      return g.subject === subject && g.type === type;
+    });
+    const sumValue = filteredArray.reduce(
+      (previousValue, currentValue) => previousValue + currentValue.value,
+      0
+    );
+    const average = sumValue / filteredArray.length;
+
+    return {
+      message: `A média da matéria ${subject} tipo ${type} é: ${average}`,
+    };
+  } catch (err) {
+    console.log(err);
+    return { error: err.message };
+  }
+}
+
+async function top3Grades(subject, type) {
+  try {
+    const jsonData = JSON.parse(await fs.readFile(global.fileNameData));
+    const filteredArray = jsonData.grades.filter((g) => {
+      return g.subject === subject && g.type === type;
+    });
+    const top3GradeValues = filteredArray
+      .sort((a, b) => b.value - a.value)
+      .slice(0, 3);
+    return top3GradeValues;
+  } catch (err) {
+    console.log(err);
+    return { error: err.message };
+  }
+}
+
+export {
+  inserirNovaGrade,
+  buscaGrade,
+  atualizarGrade,
+  deletarGrade,
+  buscaNotaAluno,
+  buscaMediaSubjectType,
+  top3Grades,
+};
--- modulo_2/trabalho_pratico_2/grades-control-api/routes/gradesRouter.js
+++ modulo_2/trabalho_pratico_2/grades-control-api/routes/gradesRouter.js
@@ -4,9 +4,17 @@ import {
   buscaGrade,
   deletarGrade,
   inserirNovaGrade,
+  buscaNotaAluno,
+  buscaMediaSubjectType,
+  top3Grades,
 } from '../controllers/gradesController.js';
 
 const gradesRouter = express.Router();
+
+// gradesRouter.use((req, res, next) => {
+//   console.log(`Access ${Date.now()}: ${req.method} ${req._parsedUrl.pathname}`);
+//   next();
+// });
 
 gradesRouter.post('/', async (req, res) => {
   const { student, subject, type, value } = req.body;
@@ -22,6 +30,21 @@ gradesRouter.put('/:id', async (req, res) => {
 
 gradesRouter.delete('/:id', async (req, res) => {
   res.send(await deletarGrade(parseInt(req.params.id)));
+});
+
+gradesRouter.get('/notaAluno', async (req, res) => {
+  const { student, subject } = req.query;
+  res.send(await buscaNotaAluno(student, subject));
+});
+
+gradesRouter.get('/mediaSubjectType', async (req, res) => {
+  const { subject, type } = req.query;
+  res.send(await buscaMediaSubjectType(subject, type));
+});
+
+gradesRouter.get('/top3Grades', async (req, res) => {
+  const { subject, type } = req.query;
+  res.send(await top3Grades(subject, type));
 });
 
 gradesRouter.get('/:id', async (req, res) => {
